88bf111fd6f39b77759f97cfb170627904e5aa95
chasacademy-sandra-larsson <111293794+chasacademy-sandra-larsson@users.noreply.github.com>
2025-03-11 15:05:23 +0100

    starter

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import express from "express";
+import cors from "cors";
+import userRoutes from "./routes/userRoutes";
+import postRoutes from "./routes/postRoutes"
+import dotenv from "dotenv";
+import { prisma } from "./config/db"
+
+dotenv.config();
+
+//const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes )
+// TODO: app.use("/comments", commentRoutes )
+
+
+const main = async () => {
+  const PORT = process.env.PORT || 3000
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
+
